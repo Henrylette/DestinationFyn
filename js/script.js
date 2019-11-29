2af6ff10bfbff72c40ef05b49058dfdd4bbadde2
Henriette <Henr8588@edu.eal.dk>
2019-11-29 13:20:03 +0100

    Slette blå section på forside

--- js/script.js
+++ js/script.js
@@ -1,8 +1,2 @@
 
 
-document.addEventListener('scroll', function(e) {
-    if (document.body.scrollTop > 400) {
-        var currScrollPos2 = document.body.scrollTop;
-        document.getElementById('test').style.opacity = -currScrollPos2/400 + 2;
-        }
-    });
