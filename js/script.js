f8edde953a096d01e60b12469584250afc9f42f6
Henriette <Henr8588@edu.eal.dk>
2019-12-07 19:43:38 +0100

    Json billede skift test

--- js/script.js
+++ js/script.js
@@ -1,34 +1,40 @@
 
 
- backgroundImages =["url(images/tema1/burger-wallpaper.jpg)", "url(images/tema2/brik-wallpaper.png)", "url(images/tema3/avocado-wallpaper.png)", "url(images/tema4/lassagne-wallpaper.png)"]; 
- 
+/*let mainObj = {};
+let showObj = function(){
+    for (let prop in mainObj){
+        console.log(prop)
+        console.log(mainObj[prop]);
+    };
+}
 
- /*Index*/
- themeImages1 = ["images/tema1/forside.png", "images/tema2/forside.png" , "images/tema3/forside.png", "images/tema4/forside.png"];
- themeImages2 = ["images/tema1/forside-2.png", "images/tema2/forside-2.png", "images/tema3/forside-2.png", "images/tema4/forside-2.png"];
- themeImages3 = ["images/tema1/ide.png", "images/tema2/ide.png", "images/tema3/ide.png", "images/tema4/ide.png"];
- themeImages4 = ["images/tema1/opret.png", "images/tema2/opret.png", "images/tema3/opret.png", "images/tema4/opret.png"];
- themeImages5 = ["images/tema1/glad.png", "images/tema2/glad.png", "images/tema3/glad.png", "images/tema4/glad.png"];
+var data = '';  
+var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			data = JSON.parse(xhttp.responseText);
+            console.log(data);
+           mainObj = data; 
+            showObj(); 
+		}
+    }
 
- var ThemeIndex = 0;
+    xhttp.open("GET", "../JSON/tema.json", true);
+    xhttp.send();
+
+    */
 
 
- function changeImage() {
-        ThemeIndex += 1;
 
-     if (ThemeIndex > backgroundImages.length - 1) {
-         ThemeIndex = 0; 
-     }
-     document.getElementById('citat-section').style.backgroundImage = backgroundImages[ThemeIndex];
-
-     
-
-     if(ThemeIndex > themeImages1.length - 1) {
-         ThemeIndex = 0;
-     }
-     document.getElementById('index-billede1').src = themeImages1[ThemeIndex]; 
-     document.getElementById('index-billede2').src = themeImages2[ThemeIndex]; 
-     document.getElementById('index-billede3').src = themeImages3[ThemeIndex]; 
-     document.getElementById('index-billede4').src = themeImages4[ThemeIndex]; 
-     document.getElementById('index-billede5').src = themeImages5[ThemeIndex]; 
- }
+   var data = '';  
+   var xhttp = new XMLHttpRequest();
+       xhttp.onreadystatechange = function(){
+           if(this.readyState == 4 && this.status == 200){
+               data = JSON.parse(xhttp.responseText);
+               console.log(data);
+           }
+       }
+   
+       xhttp.open("GET", "../JSON/tema.json", true);
+       xhttp.send();
+   
