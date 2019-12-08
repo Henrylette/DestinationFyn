169e97dd8bb1321180310fce9d0a2105c4232f25
Henriette <Henr8588@edu.eal.dk>
2019-12-08 13:09:00 +0100

    Tilføjet gem tema på hele siden

--- js/script.js
+++ js/script.js
@@ -10,31 +10,71 @@
        }
    
 
- 
- ThemeIndex = 0; 
+
+
+
 
  
+    ThemeIndex = 0; 
+
+  
+    if (ThemeIndex > 0) {
+        sessionStorage.setItem("Index", ThemeIndex);
+        console.log(sessionStorage.getItem("Index")); 
+    }
+
+    
+    function getThemeIndex(page) {
+        if ( parseInt(sessionStorage.getItem("Index")) > 0){
+            ThemeIndex = parseInt(sessionStorage.getItem("Index") -1);
+            if (page == 'index'){
+                changeImageIndex();
+            } 
+
+            if (page == 'desHygge'){
+                changeImageDesHygge(); 
+            }
+
+            if (page == 'guide'){
+                changeImageGuides();
+            }
+
+            if (page == 'com'){
+                changeImageCom();
+            }  
+        }
+    }
+ 
+
+
        function changeImageIndex() {
             ThemeIndex += 1;
     
          if (ThemeIndex > 4 - 1) {
              ThemeIndex = 0; 
          }
+
+         sessionStorage.setItem("Index", ThemeIndex); 
+
          document.getElementById('index-billede1').src = data.index[ThemeIndex].billede1;
          document.getElementById('index-billede2').src = data.index[ThemeIndex].billede2;
          document.getElementById('index-billede3').src = data.index[ThemeIndex].billede3;
          document.getElementById('index-billede4').src = data.index[ThemeIndex].billede4;
          document.getElementById('index-billede5').src = data.index[ThemeIndex].billede5;
          document.getElementById('citat-section').style.backgroundImage = data.index[ThemeIndex].bg;
-    
+         
         }
 
+        
         function changeImageDesHygge() {
             ThemeIndex += 1;
 
         if (ThemeIndex > 4 - 1) {
             ThemeIndex = 0; 
         }
+
+
+         sessionStorage.setItem("Index", ThemeIndex); 
          document.getElementById('destinationhygge-billede1').src = data.deshygge[ThemeIndex].billede1;
          document.getElementById('citat-section').style.backgroundImage = data.deshygge[ThemeIndex].bg;
 }
@@ -46,6 +86,8 @@
         if (ThemeIndex > 4 - 1) {
             ThemeIndex = 0; 
         }
+
+        sessionStorage.setItem("Index", ThemeIndex); 
         document.getElementById('guides-billede1').src = data.guides[ThemeIndex].billede1;
         document.getElementById('citat-section').style.backgroundImage = data.guides[ThemeIndex].bg;
 
@@ -58,10 +100,13 @@
     if (ThemeIndex > 4 - 1) {
         ThemeIndex = 0; 
     }
+
+    sessionStorage.setItem("Index", ThemeIndex); 
     document.getElementById('community-billede1').src = data.community[ThemeIndex].billede1;
     document.getElementById('citat-section').style.backgroundImage = data.community[ThemeIndex].bg;
 
 }
+
 
     xhttp.open("GET", "../tema.json", true);
     xhttp.send();
